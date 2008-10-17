19fa26cc301cf4a19ce8194833320af96a493214
Asheesh Laroia <asheesh@asheesh.org>
2008-10-17 11:00:23 -0700

    Add-in some special-case names we're going to want to move over

--- get_all_odepo_pages.py
+++ get_all_odepo_pages.py
@@ -1,10 +1,18 @@
 import mwclient
 from io import page2line, line2page
 
+extra_pages = [
+    'Template:Organization',
+    'Form:Organization',
+    'Category:Organization',
+    ]
+
 def main(site = 'wiki.creativecommons.org', path = '/', 
          catname = 'Category:Organization'):
     site = mwclient.Site(site, path=path)
     cat = site.Pages[catname]
+    for page_name in extra_pages:
+        print page2line(unicode(page_name))
     for page_obj in cat:
         print page2line(page_obj.name)
 
